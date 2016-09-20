291ccf1051ec3f3cff1dc6ca867e29d87783ee3f
Matthew Perkins <mfperkins@gmail.com>
2016-09-20 17:52:29 +0100

    refactoring

--- test/index.js
+++ test/index.js
@@ -1,18 +1,28 @@
-'use-strict';
+process.env.NODE_ENV = 'test';
 
-const Browser = require('zombie');
-
-Browser.localhost('example.com', 3000);
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+var expect = chai.expect;
+var app = require('../app.js');
+var http = require('http');
+var Browser = require('zombie');
 
 describe ('User visits index', function() {
-  const browser = new Browser();
 
-  before(function(done){
-    browser.visit('/', done);
+  before(function(){
+    this.server = http.createServer(app).listen(3000);
+    this.browser = new Browser({site: 'http://localhost:3000' });
+    console.log(this.browser);
   });
 
-  it('should load a welcome page', function() {
-    browser.assert.text('title', 'Hey');
+  before(function(done) {
+    this.browser.visit('/', done);
+  });
+
+  it('should load a welcome page', function(done) {
+    expet(this.browser.text('h1')).to.equal("Hello there!");
+    done();
   });
 
 });
