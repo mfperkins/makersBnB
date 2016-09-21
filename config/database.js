0466eb221dba9ec903d02ecd134d10b757dd8d9e
Matthew Perkins <mfperkins@gmail.com>
2016-09-21 15:19:36 +0100

    adding in db cleaning and travis integration

---
+++ config/database.js
@@ -0,0 +1,25 @@
+exports.DBConfig = {
+
+  "development": {
+    "username": "",
+    "password": null,
+    "database": "bnb_development",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "test": {
+    "username": "",
+    "password": null,
+    "database": "bnb_test",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "production": {
+    "username": "",
+    "password": null,
+    "database": "bnb_production",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  }
+
+};
--- test/user.js
+++ test/user.js
@@ -1,11 +1,18 @@
-// var expect    = require("chai").expect;
-// var converter = require("../models/user");
-//
-// describe ("users", function() {
-//
-//   describe ("at start", function() {
-//     it('should have a first name', function() {
-//       expect(user).toEqual(null);
-//     });
-//   });
-// });
+'use-strict';
+
+var db = require("sequelize-tools").db;
+var expect    = require("chai").expect;
+var converter = require("../models/user");
+
+describe ("users", function() {
+
+  // beforeEach(function(done) {
+  //   db.init(done);
+  // });
+
+  describe ("at start", function() {
+    it('should have a first name', function() {
+      expect(user).toEqual(null);
+    });
+  });
+});
