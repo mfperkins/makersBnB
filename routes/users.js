23aee1678116327e2b5113ba4bc117445bbd2ec5
Lawrence Dawson <lawrencedawson@hotmail.co.uk>
2016-09-21 19:21:22 +0100

    can add a user to database

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var models  = require('../models');
+var session = require('express-session');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -11,12 +13,19 @@ router.get('/sign-up', function(req, res) {
   res.render('signUp');
 });
 
-router.post('/sign-up', function(req, res) {
+router.post('/', function(req, res) {
+  models.user.create({
+      email: req.body.email,
+      password: req.body.password,
+      confirmation_password: req.body.confirmation_password
+    }).then(function() {
   res.redirect('/users/welcome');
+  });
 });
 
 router.get('/welcome', function(req, res) {
-  res.render('welcome');
+  res.render('welcome', {email: 'Hey'});
 });
+
 
 module.exports = router;
