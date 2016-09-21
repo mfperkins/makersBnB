ff5fdc534fb991c7e20011bf3215dfd5c7b941a9
Matthew Perkins <mfperkins@gmail.com>
2016-09-21 11:02:30 +0100

    crazy changes

---
+++ app/_config.js
@@ -0,0 +1,11 @@
+var config = {
+
+  "development": {
+    "database": 'postgres://localhost/bnb_development',
+  },
+  "test": {
+    "database": 'postgres://localhost/bnb_test',
+  },
+};
+
+module.exports = config;
