8a8765d8cf31708e885c6c682d3d5bc748017b4d
Lawrence Dawson <lawrencedawson@hotmail.co.uk>
2016-09-22 19:18:30 +0100

    altered email unique validation so that error is more clear

--- models/user.js
+++ models/user.js
@@ -3,34 +3,26 @@
 module.exports = function(sequelize, DataTypes) {
  var User = sequelize.define("user", {
     email: {
-        type: DataTypes.STRING,
-        validate: {
-            isUnique: function(value, next) {
-
-                User.find({
-                    where: {email: value},
-
-                })
-                    .done(function(error, user) {
-
-                        if (error)
-                            // Some unexpected error occured with the find method.
-                            return next(error);
-
-                        if (user)
-                            // We found a user with this email address.
-                            // Pass the error to the next method.
-                            return next('Email address already in use!');
-
-                        // If we got this far, the email address hasn't been used yet.
-                        // Call next with no arguments when validation is successful.
-                        next();
-
-                    });
-
+       type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isUnique: function (value, next) {
+                    var self = this;
+                    User.find({where: {email: value}})
+                        .then(function (user) {
+                            // reject if a different user wants to use the same email
+                            if (user && self.id !== user.id) {
+                                return next('Email already in use!');
+                            }
+                            return next();
+                        })
+                        .catch(function (err) {
+                            return next(err);
+                        });
+                }
             }
-        }
-    },
+        },
     password: DataTypes.STRING,
     password_confirmation: DataTypes.STRING
  });
